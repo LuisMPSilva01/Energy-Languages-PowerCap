20c954926c2abd694ffca0b73ca978366ea8fbed
Luis Silva <56602110+LuisMPSilva01@users.noreply.github.com>
2023-08-10 14:30:43 +0100

    Updated compile flags for C

--- setup.sh
+++ setup.sh
@@ -5,5 +5,10 @@ sudo apt-get install make
 ## Compilers ##
     #Ada
     sudo apt install gnat-11
+    sudo apt-get install libgmp-dev
     sudo apt-get install libapr1-dev
+
+    #C *precisa versao especifica*
+    sudo apt install build-essential
+    sudo apt-get install libpcre2-dev
 
