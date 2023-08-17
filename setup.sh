4ad968a97d8edd208af7693152eccdc716e30f91
simaocunha71 <simaopscunha@outlook.pt>
2023-08-17 11:19:33 +0100

    fix swift compiler

--- setup.sh
+++ setup.sh
@@ -21,8 +21,10 @@ sudo apt-get install make
     #Swift
     sudo apt-get install swift
     sudo apt install clang libicu-dev libxml2 git libgmp-dev
+    sudo apt install binutils gnupg2 libc6-dev libcurl4 libedit2 libgcc-9-dev libsqlite3-0 libstdc++-9-dev libxml2 libz3-dev pkg-config tzdata zlib1g-dev
     wget https://download.swift.org/swift-5.8-branch/ubuntu2204/swift-5.8-DEVELOPMENT-SNAPSHOT-2023-03-17-a/swift-5.8-DEVELOPMENT-SNAPSHOT-2023-03-17-a-ubuntu22.04.tar.gz
     tar xzf swift-5.8-DEVELOPMENT-SNAPSHOT-2023-03-17-a-ubuntu22.04.tar.gz
+    sudo mv swift-5.8-DEVELOPMENT-SNAPSHOT-2023-03-17-a-ubuntu22.04 /opt/swift
     rm -rf *.gz swift-5.8-DEVELOPMENT-SNAPSHOT-2023-03-17-a-ubuntu22.04/
     echo 'export PATH=/opt/swift/usr/bin:$PATH' >> ~/.bashrc
     source ~/.bashrc
