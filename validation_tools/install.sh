f79692f489e9540c3d6334e9197449c4c44b7bf7
Simão Pedro Sá Cunha <61991247+simaocunha71@users.noreply.github.com>
2024-02-05 22:29:30 +0000

    repositorio python para compressao de imagens

--- validation_tools/install.sh
+++ validation_tools/install.sh
@@ -3,3 +3,6 @@ sudo apt install graphviz
 
 #PlantUML installation
 wget https://github.com/plantuml/plantuml/releases/download/v1.2024.0/plantuml-1.2024.0.jar
+
+#optimize-images pip3 library installation
+pip3 install pillow optimize-images
