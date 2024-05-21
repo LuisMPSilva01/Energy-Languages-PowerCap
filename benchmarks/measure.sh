50fcff8bd08eb9663f7032c20b5db8d3f18f51b1
Simão Pedro Sá Cunha <61991247+simaocunha71@users.noreply.github.com>
2024-05-21 21:01:19 +0100

    script funcional dos benchmarks

--- benchmarks/measure.sh
+++ benchmarks/measure.sh
@@ -1,60 +1,55 @@
 #!/bin/bash
 
-NTIMES=2
-#NTIMES=1
+NTIMES=1
 
 # Compile sensors which will be used to calculate cool temperature
-cd ../RAPL || exit
+cd RAPL || exit
 gcc -shared -o sensors.so sensors.c
 cd ..
 
 # Update the temperature value
-cd Utils/
-sudo python3 temperatureUpdate.py
-cd ..
+sudo python3 Utils/temperatureUpdate.py
 
-echo "Benchmark,Program,PowerLimit,Package,Core,GPU,DRAM,Time,Temperature,Memory" > measurements_benchmarks.csv
+# Initialize the final measurements CSV
+echo "Benchmark,Program,PowerLimit,Package,Core,GPU,DRAM,Time,Temperature,Memory" > measurements.csv
+
+# Lists of programs to run
+programs_pyperformance=("2to3" "async_generators" "async_tree" "async_tree_cpu_io_mixed" "async_tree_cpu_io_mixed_tg" "async_tree_eager" "async_tree_eager_cpu_io_mixed" "async_tree_eager_cpu_io_mixed_tg" "async_tree_eager_io" "async_tree_eager_io_tg" "async_tree_eager_memoization" "async_tree_eager_memoization_tg" "async_tree_eager_tg" "async_tree_io" "async_tree_io_tg" "async_tree_memoization" "async_tree_memoization_tg" "async_tree_tg" "asyncio_tcp" "asyncio_tcp_ssl" "asyncio_websockets" "chameleon" "chaos" "comprehensions" "concurrent_imap" "coroutines" "coverage" "crypto_pyaes" "dask" "deepcopy" "deltablue" "django_template" "docutils" "dulwich_log" "fannkuch" "float" "gc_collect" "gc_traversal" "generators" "genshi" "go" "hexiom" "html5lib" "json_dumps" "json_loads" "logging" "mako" "mdp" "meteor_contest" "nbody" "nqueens" "pathlib" "pickle" "pickle_dict" "pickle_list" "pickle_pure_python" "pidigits" "pprint" "pyflate" "python_startup" "python_startup_no_site" "raytrace" "regex_compile" "regex_dna" "regex_effbot" "regex_v8" "richards" "richards_super" "scimark" "spectral_norm" "sqlalchemy_declarative" "sqlalchemy_imperative" "sqlglot" "sqlglot_optimize" "sqlglot_parse" "sqlglot_transpile" "sqlite_synth" "sympy" "telco" "tomli_loads" "tornado_http" "typing_runtime_protocols" "unpack_sequence" "unpickle" "unpickle_list" "unpickle_pure_python" "xml_etree")
+programs_nofib=("bspt" "compress2" "fluid" "gg" "hpg" "linear" "parser" "reptile" "smallpt" "anna" "cacheprof" "eff" "fulsom" "grep" "infer" "maillist" "pic" "rsa" "symalg" "ben-raytrace" "compress" "fem" "gamteb" "hidden" "lift" "mkhprog" "prolog" "scs" "veritas")
+programs_dacapo=("avrora" "batik" "biojava" "cassandra" "eclipse" "fop" "graphchi" "h2" "h2o" "jme" "jython" "kafka" "luindex" "lusearch" "pmd" "spring" "sunflow" "tomcat" "tradebeans" "tradesoap" "xalan" "zxing")
 
 # Loop over power limit values
-for limit in 10 25; do
-    cd Utils/
-    sudo python3 raplCapUpdate.py "$limit" ../RAPL/main.c
-    cd ..
+for limit in -1 2 10 15 25; do
+    sudo python3 Utils/raplCapUpdate.py "$limit" RAPL/main.c
 
     # Make RAPL lib
     cd RAPL/ || exit
-    #sudo rm sensors.so
     sudo make
     cd ..
 
-    cd benchmarks/dacapo || exit
-    for problem in avrora; do
-        sudo ../../RAPL/main "sudo /usr/lib/jvm/jdk-20/bin/java -jar dacapo-23.11-chopin.jar $problem" dacapo $problem "$NTIMES"
-        # Specify the input file name
-        file="measurements_benchmarks.csv"
-        sudo tail -n +2 "$file" >>measurements_benchmarks.csv
+    # Run each Pyperformance program and append results to the final CSV file
+    for program in "${programs_pyperformance[@]}"; do
+        cd pyperformance || exit
+        sudo ../RAPL/main "sudo /usr/local/bin/pyperformance run -b $program" pyperformance "$program" "$NTIMES"
+        sudo tail -n +2 measurements.csv >> ../measurements.csv
+        cd ..
     done
-    cd ../..
 
-    #cd benchmarks/nofib || exit
-    #for problem in bspt compress2; do
-    #    sudo ../../RAPL/main "sudo make -C nofib_repo/real/$problem/" nofib "$problem" "$NTIMES"
-    #    # Specify the input file name
-    #    file="measurements_benchmarks.csv"
-    #    sudo tail -n +2 "$file" >>measurements_benchmarks.csv
-    #done
+    # Run each Nofib program and append results to the final CSV file
+    for program in "${programs_nofib[@]}"; do
+        cd nofib_repo || exit
+        sudo ../RAPL/main "sudo make -C nofib/real/$program/" nofib "$program" "$NTIMES"
+        sudo tail -n +2 measurements.csv >> ../measurements.csv
+        cd ..
+    done
 
-    #cd ../..
-
-    #cd benchmarks/pyperformance || exit
-    #for problem in 2to3 async_generators; do
-    #    sudo ../../RAPL/main "sudo ~/.local/bin/pyperformance run -b $problem" pyperformance "$problem" "$NTIMES"
-    #    # Specify the input file name
-    #    file="measurements_benchmarks.csv"
-    #    sudo tail -n +2 "$file" >>measurements_benchmarks.csv
-    #done
-    #cd ../..
-
+    # Run each Dacapo program and append results to the final CSV file
+    for program in "${programs_dacapo[@]}"; do
+        cd dacapo || exit
+        sudo ../RAPL/main "sudo /usr/lib/jvm/jdk-20/bin/java -jar dacapo-23.11-chopin.jar $program" dacapo "$program" "$NTIMES"
+        sudo tail -n +2 measurements.csv >> ../measurements.csv
+        cd ..
+    done
 done
 
 cd RAPL/ || exit
@@ -62,4 +57,3 @@ sudo make clean
 cd ..
 
 #sudo reboot
-
